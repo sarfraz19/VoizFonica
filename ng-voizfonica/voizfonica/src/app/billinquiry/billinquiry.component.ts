1fbf1990ed33602296b62f53f1c33e376323bf37
sarfraz19 <fakrudeen998@gmail.com>
2019-07-06 12:06:31 +0530

    commitings

---
+++ ng-voizfonica/voizfonica/src/app/billinquiry/billinquiry.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-billinquiry',
+  templateUrl: './billinquiry.component.html',
+  styleUrls: ['./billinquiry.component.css']
+})
+export class BillinquiryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
