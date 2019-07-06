207df4688bed7986faa10a454ab846006afd21c8
priyaakrishvish <priyaakrishvish@gmail.com>
2019-07-06 15:55:05 +0530

    bill inquiry table aligned

--- ng-voizfonica/voizfonica/src/app/billinquiry/billinquiry.component.ts
+++ ng-voizfonica/voizfonica/src/app/billinquiry/billinquiry.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billinquiry.component.css']
 })
 export class BillinquiryComponent implements OnInit {
-
+bill_details:any[];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  fn(){
+    this.bill_details=[
+      {
+        left:'VoizFonica Number',
+        right:'9873547692'
+      },
+      {
+         left:'Bill Number',
+        right:'74968736'
+      },
+      {
+        left:'Bill Date',
+        right:'23-07-2019'
+      },
+      {
+        left:'Bill period',
+        right:'15-07-2019 to 20-07-2019'
+      },
+      {
+        left:'Pay by Date',
+        right:'30-07-2019'
+      },
+      {
+        left:'Bill Amount',
+        right:'Rs.200'
+      }
+    ];
   }
 
 }
