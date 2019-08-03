38c4a931c225227aa3937b504cefdd4614cfb602
vishnupriyaaiprimed <vishnupriyaa.iprimed@gmail.com>
2019-08-03 17:28:31 +0530

    createaccount front end validation

--- ng-voizfonica/voizfonica/src/app/createaccount/createaccount.component.ts
+++ ng-voizfonica/voizfonica/src/app/createaccount/createaccount.component.ts
@@ -8,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class CreateaccountComponent implements OnInit {
 
   var1="hi";
-  name1:string;
-  phone_num1:string;
-  pw1:string;
-  retype_pw1:string;
+  isBtnClicked=false;
+
+  name_var:string;
+  phone_num_var:string;
+  pw_var:string;
+  retype_pw_var:string;
 
   constructor() {}
 
@@ -21,5 +23,45 @@ export class CreateaccountComponent implements OnInit {
   save(){
     this.var1="hello";
   }
+
+  name_match(str):boolean
+  {
+    if(str!=null && str!='')
+    {
+      str=str.trim()
+      var reg=new RegExp('^[a-zA-Z\ ]+$');
+      var name_val=reg.test(str); 
+      return name_val;
+    }
+    return true;
+  }
+
+  phone_num_match(num):boolean
+  {
+    if(num!=null)
+    {
+      var str:string;
+      str=num;
+      var reg=new RegExp('^[0-9]{10}$');
+      var phone_num_val=reg.test(str); 
+      return phone_num_val;
+    }
+    return true;
+  }
+
+  regForm_valid():boolean
+  {
+    if(this.name_var!=null && this.name_var!=''
+    && this.phone_num_var!=null && this.phone_num_var!=''
+    && this.pw_var!=null && this.pw_var!=''
+    && this.retype_pw_var!=null && this.retype_pw_var!=''
+    && this.name_match(this.name_var) 
+    && this.phone_num_match(this.phone_num_var) 
+    && this.pw_var==this.retype_pw_var)
+      return true;
+    else
+      return false;
+  }
+
 
 }
