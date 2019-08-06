86bf8b384e73463e6ecb3020409acc108a88dd33
soundary219 <soundaryajaishankar@gmail.com>
2019-08-06 19:07:45 +0530

    newcon

--- ng-voizfonica/voizfonica/src/app/newcon/newcon.component.ts
+++ ng-voizfonica/voizfonica/src/app/newcon/newcon.component.ts
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewconComponent implements OnInit {
 
+  name_var:string;
+  address_var:string;
+  pincode_var:string;
+  email_var:string;
+  selectnum_var:string;
+  preposdon_var:string;
+  options=["Prepaid","Postpaid","Dongle"];
+  selectplan_var:string;
+  kycdate_var:string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  name_match(str):boolean
+  {
+    if(str!=null && str!='')
+    {
+      str=str.trim();
+      var reg=new RegExp('^[a-zA-Z\ ]+$');
+      return reg.test(str);
+    }
+    return true;
+  }
+
+  pincode_match(num):boolean
+  {
+    if(num!=null)
+    {
+      var str:string;
+      str=num;
+      var reg=new RegExp('^[0-9]{6}$');
+      return reg.test(str);
+    }
+    return true;
+  }
+
+  email_match(str):boolean
+  {
+    if(str!=null && str!='')
+    {
+      str=str.trim();
+      var reg=new RegExp('^[a-zA-Z0-9]+([\.-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([\.-_][a-zA-Z0-9]+)*\.[a-zA-Z]{2,3}$');
+      return reg.test(str);
+    }
+    return true;
+  }
+  
 }
